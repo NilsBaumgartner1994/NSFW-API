5e4d76deffd188654060ae4d58cf9302b2b78dba
Nils <nilsbaumgartner1994@gmail.com>
2021-01-27 10:23:25 +0100

    Exported CustomControllers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // this should be the entry point to your library
 module.exports = {
     ServerAPI: require('./ServerAPI').default, // eslint-disable-line global-require
+    CustomControllers: require('./controllers/CustomControllers').default, // eslint-disable-line global-require
 };
