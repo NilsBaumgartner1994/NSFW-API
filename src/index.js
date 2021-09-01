db3558e6ec17ceea54afef8aa6e0eaa052ab95a2
Nils <nilsbaumgartner1994@gmail.com>
2021-09-01 18:30:35 +0200

    Fixed export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // this should be the entry point to your library
-import AuthConfigList from "./auth/AuthConfigList";
-
 module.exports = {
     ServerAPI: require('./ServerAPI').default, // eslint-disable-line global-require
     CustomControllers: require('./controllers/CustomControllers').default, // eslint-disable-line global-require
